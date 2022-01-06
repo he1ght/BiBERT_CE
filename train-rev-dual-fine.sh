8aa0e0b0ea21a4812ec880b6a7ddec3c6130a950
HeeSeung Jung <heesng.jung@gmail.com>
2022-01-06 17:31:32 +0900

    reverse train shell

--- train-rev-dual-fine.sh
+++ train-rev-dual-fine.sh
@@ -3,7 +3,7 @@
 TEXT=./download_prepare_reverse/data_mixed/
 SAVE_DIR=./models/reverse/dual/
 
-CUDA_VISIBLE_DEVICES=0,1,2,3 fairseq-train ${TEXT}de-en-databin/ --arch transformer_iwslt_de_en --ddp-backend no_c10d --optimizer adam --adam-betas '(0.9, 0.98)' \
+CUDA_VISIBLE_DEVICES=0,1,2,3 fairseq-train ${TEXT}en-de-databin/ --arch transformer_iwslt_de_en --ddp-backend no_c10d --optimizer adam --adam-betas '(0.9, 0.98)' \
 --clip-norm 1.0 --lr 0.0004 --lr-scheduler inverse_sqrt --warmup-updates 1000 --warmup-init-lr 1e-07 --dropout 0.3 \
 --weight-decay 0.00002 --criterion label_smoothed_cross_entropy --label-smoothing 0.1 --max-tokens 2048 --update-freq 4 \
 --attention-dropout 0.1 --activation-dropout 0.1 --max-epoch 75 --save-dir ${SAVE_DIR}  --encoder-embed-dim 768 --decoder-embed-dim 768 \
@@ -16,7 +16,7 @@ CUDA_VISIBLE_DEVICES=0,1,2,3 fairseq-train ${TEXT}de-en-databin/ --arch transfor
 TEXT=./download_prepare_reverse/data_mixed_ft/
 SAVE_DIR=./models/reverse/dual-ft/
 
-CUDA_VISIBLE_DEVICES=0,1,2,3 fairseq-train ${TEXT}de-en-databin/ --arch transformer_iwslt_de_en --ddp-backend no_c10d --optimizer adam --adam-betas '(0.9, 0.98)' \
+CUDA_VISIBLE_DEVICES=0,1,2,3 fairseq-train ${TEXT}en-de-databin/ --arch transformer_iwslt_de_en --ddp-backend no_c10d --optimizer adam --adam-betas '(0.9, 0.98)' \
 --clip-norm 1.0 --lr 0.0004 --lr-scheduler inverse_sqrt --warmup-updates 1000 --warmup-init-lr 1e-07 --dropout 0.3 \
 --weight-decay 0.00002 --criterion label_smoothed_cross_entropy --label-smoothing 0.1 --max-tokens 2048 --update-freq 4 \
 --attention-dropout 0.1 --activation-dropout 0.1 --max-epoch 20 --save-dir ${SAVE_DIR}  --encoder-embed-dim 768 --decoder-embed-dim 768 \
