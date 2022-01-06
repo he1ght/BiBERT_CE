b86a9d461c8457c221c4c3a5ab9e93b76dbf2863
HeeSeung Jung <heesng.jung@gmail.com>
2022-01-06 15:48:30 +0900

    reverse data

--- download_prepare_reverse/download_and_prepare_data.sh
+++ download_prepare_reverse/download_and_prepare_data.sh
@@ -21,7 +21,7 @@ perl mosesdecoder/scripts/tokenizer/detokenizer.perl -l en -q < data_desubnmt/te
 perl mosesdecoder/scripts/tokenizer/detokenizer.perl -l en -q < data_desubnmt/test.de > data_demose/test.de
 
 ## train 8K tokenizer for ordinary translation:
-cat data_demose/train.en data_demose/valid.en data_demose/test.en | shuf > data_demose/train.all
+cat data_demose/train.de data_demose/valid.de data_demose/test.de | shuf > data_demose/train.all
 mkdir 8k-vocab-models
 python vocab_trainer.py --data data_demose/train.all --size 8000 --output 8k-vocab-models
 
@@ -47,7 +47,7 @@ done
 
 for prefix in "valid" "test" "train" ;
 do
-    python transform_tokenize.py --input data_demose/${prefix}.en --output 8k_tok/${prefix}.en --pretrained_model 8k-vocab-models
+    python transform_tokenize.py --input data_demose/${prefix}.de --output 8k_tok/${prefix}.de --pretrained_model 8k-vocab-models
 done
 
 
@@ -61,32 +61,32 @@ done
 
 
 mkdir data   # for one-way translation data
-cp bibert_tok/*.de data/ 
-cp 8k_tok/*.en data/
+cp bibert_tok/*.en data/
+cp 8k_tok/*.de data/
 
 mkdir data_mixed_ft # for dual-directional fine-tuning data. we first preprocess this because it will be easier to finish
-cp bibert_tok/*.de data_mixed_ft/
-cp 12k_tok/*.en data_mixed_ft/
+cp bibert_tok/*.en data_mixed_ft/
+cp 12k_tok/*.de data_mixed_ft/
 
 mkdir data_mixed # preprocess dual-directional data
 
 cd data_mixed
-cat ../bibert_tok/train.en ../bibert_tok/train.de > train.all.en
-cat ../12k_tok/train.de ../12k_tok/train.en > train.all.de
-paste -d '@@@' train.all.en /dev/null /dev/null train.all.de | shuf > train.all
-cat train.all | awk -F'@@@' '{print $1}' > train.de
-cat train.all | awk -F'@@@' '{print $2}' > train.en
+cat ../bibert_tok/train.de ../bibert_tok/train.en > train.all.de
+cat ../12k_tok/train.en ../12k_tok/train.de > train.all.en
+paste -d '@@@' train.all.de /dev/null /dev/null train.all.en | shuf > train.all
+cat train.all | awk -F'@@@' '{print $1}' > train.en
+cat train.all | awk -F'@@@' '{print $2}' > train.de
 rm train.all*
 
-cat ../bibert_tok/valid.en ../bibert_tok/valid.de > valid.all.en
-cat ../12k_tok/valid.de ../12k_tok/valid.en > valid.all.de
-paste -d '@@@' valid.all.en /dev/null /dev/null valid.all.de | shuf > valid.all
-cat valid.all | awk -F'@@@' '{print $1}' > valid.de
-cat valid.all | awk -F'@@@' '{print $2}' > valid.en
+cat ../bibert_tok/valid.de ../bibert_tok/valid.en > valid.all.de
+cat ../12k_tok/valid.en ../12k_tok/valid.de > valid.all.en
+paste -d '@@@' valid.all.de /dev/null /dev/null valid.all.en | shuf > valid.all
+cat valid.all | awk -F'@@@' '{print $1}' > valid.en
+cat valid.all | awk -F'@@@' '{print $2}' > valid.de
 rm valid.all*
 
-cp ../bibert_tok/test.de .
-cp ../12k_tok/test.en .
+cp ../bibert_tok/test.en .
+cp ../12k_tok/test.de .
 cd ..
 
 
@@ -103,18 +103,18 @@ python get_vocab.py --tokenizer 12k-vocab-models --output data_mixed_ft/tgt_voca
 
 ## fairseq preprocess
 TEXT=data
-fairseq-preprocess --source-lang de --target-lang en  --trainpref $TEXT/train --validpref $TEXT/valid \
---testpref $TEXT/test --destdir ${TEXT}/de-en-databin --srcdict $TEXT/src_vocab.txt \
+fairseq-preprocess --source-lang en --target-lang de  --trainpref $TEXT/train --validpref $TEXT/valid \
+--testpref $TEXT/test --destdir ${TEXT}/en-de-databin --srcdict $TEXT/src_vocab.txt \
 --tgtdict $TEXT/tgt_vocab.txt --vocab_file $TEXT/src_vocab.txt --workers 25
 
 TEXT=data_mixed
-fairseq-preprocess --source-lang de --target-lang en  --trainpref $TEXT/train --validpref $TEXT/valid \
---testpref $TEXT/test --destdir ${TEXT}/de-en-databin --srcdict $TEXT/src_vocab.txt \
+fairseq-preprocess --source-lang en --target-lang de  --trainpref $TEXT/train --validpref $TEXT/valid \
+--testpref $TEXT/test --destdir ${TEXT}/en-de-databin --srcdict $TEXT/src_vocab.txt \
 --tgtdict $TEXT/tgt_vocab.txt --vocab_file $TEXT/src_vocab.txt --workers 25
 
 TEXT=data_mixed_ft
-fairseq-preprocess --source-lang de --target-lang en  --trainpref $TEXT/train --validpref $TEXT/valid \
---testpref $TEXT/test --destdir ${TEXT}/de-en-databin --srcdict $TEXT/src_vocab.txt \
+fairseq-preprocess --source-lang en --target-lang de  --trainpref $TEXT/train --validpref $TEXT/valid \
+--testpref $TEXT/test --destdir ${TEXT}/en-de-databin --srcdict $TEXT/src_vocab.txt \
 --tgtdict $TEXT/tgt_vocab.txt --vocab_file $TEXT/src_vocab.txt --workers 25
 
 ## remove useless files
