e17f45ecabee345263d7f39f159f1c21dc276e6f
HeeSeung Jung <heesng.jung@gmail.com>
2022-01-06 15:08:53 +0900

    generater is modified

--- generate-dual-fine.sh
+++ generate-dual-fine.sh
@@ -6,4 +6,4 @@ PRE=./download_prepare/12k-vocab-models/
 CUDA_VISIBLE_DEVICES=0 fairseq-generate \
 ${STPATH} --path ${MODELPATH}checkpoint_best.pt --bpe bert --pretrained_bpe ${PRE} --pretrained_bpe_src ${PRE_SRC} \
 --beam 4 --lenpen 0.6 --remove-bpe --vocab_file=${STPATH}/dict.en.txt \
---max-len-a 1 --max-len-b 50|tee ${STPATH}/generate.out
+--max-len-a 1 --max-len-b 50|tee ${STPATH}/generate-dual-fine.out
